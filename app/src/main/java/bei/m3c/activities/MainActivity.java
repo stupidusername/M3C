8120501f6b251009254f345d7aec14c8f8860d10
nico <nicolas.p.suarez@gmail.com>
2016-07-07 10:47:34 -0300

    added tabs

--- app/src/main/java/bei/m3c/activities/MainActivity.java
+++ app/src/main/java/bei/m3c/activities/MainActivity.java
@@ -1,14 +1,72 @@
 package bei.m3c.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
-import bei.m3c.R;
 
-public class MainActivity extends Activity {
+import bei.m3c.R;
+import bei.m3c.fragments.MusicFragment;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MusicFragment(), getString(R.string.music_title));
+        adapter.addFragment(new MusicFragment(), getString(R.string.tv_title));
+        adapter.addFragment(new MusicFragment(), getString(R.string.lights_title));
+        adapter.addFragment(new MusicFragment(), getString(R.string.ac_title));
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }

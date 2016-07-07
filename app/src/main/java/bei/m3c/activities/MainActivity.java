2153f1a01915e1e19b9afadaa8b825ad18c25442
nico <nicolas.p.suarez@gmail.com>
2016-07-07 06:52:47 -0300

    created main activity

---
+++ app/src/main/java/bei/m3c/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package bei.m3c.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import bei.m3c.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

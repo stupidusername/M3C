80d77c0acb7215f0528d8e23b291109908707012
nico <nicolas.p.suarez@gmail.com>
2016-07-22 16:27:42 -0300

    changed tv layout

--- app/src/main/java/bei/m3c/fragments/TVFragment.java
+++ app/src/main/java/bei/m3c/fragments/TVFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
@@ -21,6 +22,9 @@ import bei.m3c.models.ChannelCategory;
  */
 public class TVFragment extends Fragment {
 
+    // Max number in number grid layout
+    private static final int NUMBER_MAX = 9;
+
     private ListView listView;
     private ImageButton powerButton;
     private ImageButton plusButton;
@@ -29,6 +33,8 @@ public class TVFragment extends Fragment {
     private ImageButton downButton;
     private ImageButton muteButton;
     private Button infoButton;
+    private GridLayout numberGridLayout;
+    private Button[] numberButtons;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,6 +55,16 @@ public class TVFragment extends Fragment {
         downButton = (ImageButton) view.findViewById(R.id.tv_down_button);
         muteButton = (ImageButton) view.findViewById(R.id.tv_mute_button);
         infoButton = (Button) view.findViewById(R.id.tv_info_button);
+        numberGridLayout = (GridLayout) view.findViewById(R.id.tv_number_gridlayout);
+
+        // Set up number buttons
+        numberButtons = new Button[NUMBER_MAX + 1];
+        for (int number = 0; number <= NUMBER_MAX; number++) {
+            String viewName = "tv_" + number + "_button";
+            int viewId = getResources().getIdentifier(viewName, "id", getContext().getPackageName());
+            numberButtons[number] = (Button) view.findViewById(viewId);
+            ThemeHelper.setButtonTheme(numberButtons[number]);
+        }
 
         // Load demo data to the channel category list view
         ArrayList<ChannelCategory> categories = new ArrayList<>();
@@ -70,5 +86,6 @@ public class TVFragment extends Fragment {
         ThemeHelper.setImageButtonTheme(downButton);
         ThemeHelper.setImageButtonTheme(muteButton);
         ThemeHelper.setButtonTheme(infoButton);
+        ThemeHelper.setColorStateListTheme(numberGridLayout);
     }
 }

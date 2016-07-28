0dcb89fcce05ff5c0c3c71bd92da2520eab9e7d4
nico <nicolas.p.suarez@gmail.com>
2016-07-28 08:39:41 -0300

    added music fragment event subscriber

--- app/src/main/java/bei/m3c/adapters/RadioAdapterBase.java
+++ app/src/main/java/bei/m3c/adapters/RadioAdapterBase.java
@@ -1,43 +1,43 @@
 package bei.m3c.adapters;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
-import java.util.List;
 
 import bei.m3c.R;
 import bei.m3c.helpers.ThemeHelper;
 import bei.m3c.models.Radio;
 
-public class RadioAdapter extends ArrayAdapter<Radio> {
+public class RadioAdapterBase extends BaseListAdapter<Radio> {
 
+    private final LayoutInflater layoutInflater;
     private TextView textView;
 
-    public RadioAdapter(Context context, int resource, List<Radio> items) {
-        super(context, resource, items);
+    public RadioAdapterBase(LayoutInflater layoutInflater) {
+        this.layoutInflater = layoutInflater;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         if (convertView == null) {
-            LayoutInflater vi;
-            vi = LayoutInflater.from(getContext());
-            convertView = vi.inflate(R.layout.listview_row, null);
+            convertView = layoutInflater.inflate(R.layout.listview_row, null);
             ThemeHelper.setColorStateListTheme(convertView);
         }
 
-        Radio p = getItem(position);
+        Radio radio = getItem(position);
 
-        if (p != null) {
+        if (radio != null) {
             textView = (TextView) convertView.findViewById(R.id.listview_row_text);
 
             if (textView != null) {
-                textView.setText(p.title);
+                textView.setText(radio.title);
             }
         }
 

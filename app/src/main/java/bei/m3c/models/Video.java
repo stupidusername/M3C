d59aebab28cbffd897687e965a6144ba018db11f
nico <nicolas.p.suarez@gmail.com>
2016-10-15 09:39:32 -0300

    added video models

---
+++ app/src/main/java/bei/m3c/models/Video.java
@@ -0,0 +1,8 @@
+package bei.m3c.models;
+
+public class Video {
+
+    public int id;
+    public String title;
+    public String videoUrl;
+}
---
+++ app/src/main/java/bei/m3c/models/VideoCategory.java
@@ -0,0 +1,12 @@
+package bei.m3c.models;
+
+public class VideoCategory {
+
+    public int id;
+    public String title;
+
+    public VideoCategory(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}

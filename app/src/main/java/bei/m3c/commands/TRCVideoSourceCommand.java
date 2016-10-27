ca64536c0b19f5cbbd28228268ff6d8352d9dbd5
nico <nicolas.p.suarez@gmail.com>
2016-10-27 17:19:38 -0300

    added video source command

---
+++ app/src/main/java/bei/m3c/commands/TRCVideoSourceCommand.java
@@ -0,0 +1,11 @@
+package bei.m3c.commands;
+
+public class TRCVideoSourceCommand extends BaseCommand {
+
+    public static final String TAG = "TRCVideoSourceCommand";
+    public static final byte VALUE = 31;
+
+    public TRCVideoSourceCommand() {
+        super(TAG, VALUE);
+    }
+}

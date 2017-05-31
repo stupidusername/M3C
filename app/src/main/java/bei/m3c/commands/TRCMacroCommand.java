e71607f30db8633971f6908654869e8154e50a3b
nico <nicolas.p.suarez@gmail.com>
2017-05-31 01:54:44 -0300

    changed macro command order

--- app/src/main/java/bei/m3c/commands/TRCMacroCommand.java
+++ app/src/main/java/bei/m3c/commands/TRCMacroCommand.java
@@ -6,11 +6,20 @@ public class TRCMacroCommand extends BaseCommand {
 
     public static final String TAG = "TRCMacroCommand";
     public static final byte VALUE = 18;
+    public static final int MACRO_LENGTH = 3;
 
     public int number;
 
     public TRCMacroCommand(int number) {
-        super(TAG, VALUE, FormatHelper.asNumberByteArray(number));
+        super(TAG, VALUE, getMacro(FormatHelper.asNumberByteArray(number)));
         this.number = number;
+    }
+
+    private static byte[] getMacro(byte[] number) {
+        byte[] macro = new byte[MACRO_LENGTH];
+        for (int i = 0; i < MACRO_LENGTH; i++) {
+            macro[MACRO_LENGTH - i - 1] = number.length > i ? number[i] : 0;
+        }
+        return macro;
     }
 }
